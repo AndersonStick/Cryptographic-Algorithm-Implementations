c39e190403ed54cd7a5228dca3fdd53435374d48
Anderson Barrera <andersonbar10@gmail.com>
2024-04-10 15:45:58 -0500

    AES comentado

--- AES/main.py
+++ AES/main.py
@@ -1,36 +1,36 @@
-import os, base64
-from pyaes import AESModeOfOperationCTR
+import os, base64  # Importa los módulos os y base64
+from pyaes import AESModeOfOperationCTR  # Importa la clase AESModeOfOperationCTR del módulo pyaes
 
-imageName = "crypto"
-key = "1234567890123456"
+imageName = "crypto"  # Nombre del archivo de imagen
+key = "1234567890123456"  # Clave de cifrado AES
 
 # ----------------------------------------------------------------
 
-imageName = imageName.encode()
-key = key.encode()
+imageName = imageName.encode()  # Convierte el nombre de la imagen a bytes
+key = key.encode()  # Convierte la clave a bytes
 
-with open(imageName.decode() + ".png", mode='rb') as file:
-# with open(imageName, mode='rb') as file:
+with open(imageName.decode() + ".png", mode='rb') as file:  # Abre el archivo de imagen en modo lectura binaria
+# with open(imageName, mode='rb') as file:  # Esta línea podría ser una opción alternativa para abrir el archivo
     
-    imageRaw = file.read()
-    decryptedImage = imageName.decode() + "Decrypted.png"
+    imageRaw = file.read()  # Lee el contenido del archivo de imagen
+    decryptedImage = imageName.decode() + "Decrypted.png"  # Nombre del archivo de imagen desencriptado
 
-    print("\nImagen original representada en bits (Bits 1 al 64): \n", imageRaw[0:64], "\n")
+    print("\nImagen original representada en bits (Bits 1 al 64): \n", imageRaw[0:64], "\n")  # Imprime los primeros 64 bits de la imagen original
 
-    encrypted = AESModeOfOperationCTR(key).encrypt(imageRaw)
-    print("Imagen encriptada con AES (Bits 1 al 64): \n", encrypted[0:64], "\n")
+    encrypted = AESModeOfOperationCTR(key).encrypt(imageRaw)  # Encripta la imagen utilizando AES
+    print("Imagen encriptada con AES (Bits 1 al 64): \n", encrypted[0:64], "\n")  # Imprime los primeros 64 bits de la imagen encriptada
 
-    encoded64 = base64.standard_b64encode(encrypted)
-    print("Codificacion con base64 (Bits 1 al 64): \n", encoded64[0:64], "\n")
+    encoded64 = base64.standard_b64encode(encrypted)  # Codifica la imagen encriptada en base64
+    print("Codificacion con base64 (Bits 1 al 64): \n", encoded64[0:64], "\n")  # Imprime los primeros 64 bits de la codificación base64
 
-    decoded64 = base64.standard_b64decode(encoded64)
-    print("Decodificacion con base64 (Bits 1 al 64): \n", decoded64[0:64], "\n")
+    decoded64 = base64.standard_b64decode(encoded64)  # Decodifica la imagen encriptada en base64
+    print("Decodificacion con base64 (Bits 1 al 64): \n", decoded64[0:64], "\n")  # Imprime los primeros 64 bits de la decodificación base64
 
-    decrypted = AESModeOfOperationCTR(key).decrypt(decoded64)
-    print("Imagen desencriptada con AES (Bits 1 al 64): \n", decrypted[0:64])
+    decrypted = AESModeOfOperationCTR(key).decrypt(decoded64)  # Desencripta la imagen utilizando AES
+    print("Imagen desencriptada con AES (Bits 1 al 64): \n", decrypted[0:64])  # Imprime los primeros 64 bits de la imagen desencriptada
     
-    with open(decryptedImage, "wb") as file:
-        file.write(decrypted)
-        file.close()
+    with open(decryptedImage, "wb") as file:  # Abre un archivo para escribir el resultado desencriptado en modo binario
+        file.write(decrypted)  # Escribe la imagen desencriptada en el archivo
+        file.close()  # Cierra el archivo
 
-    os.system("start " + decryptedImage)
+    os.system("start " + decryptedImage)  # Abre el archivo de imagen desencriptado utilizando el programa predeterminado del sistema
